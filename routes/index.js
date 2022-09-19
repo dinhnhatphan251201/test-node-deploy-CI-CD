3295ca88f693f2a97383710db0f6319e0e0cb2f9
dinhnhatphan251201 <dinhnhatphan251201@gmail.com>
2022-09-20 00:13:06 +0700

    update 3

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "Hello jenkins develop 2" });
+    res.render("index", { title: "Hello jenkins develop 3" });
 });
 
 module.exports = router;
