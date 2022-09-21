e40fbd9b94dbe95269193d8d22f612dbbf7c3aea
dinhnhatphan251201 <dinhnhatphan251201@gmail.com>
2022-09-21 10:09:08 +0700

    phan dinh nhat

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "Hello jenkins develop 1 4 dfwqefwef" });
+    res.render("index", { title: "Hello Phan Dinh Nhar" });
 });
 
 module.exports = router;
