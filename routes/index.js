35dba25ae1c40aca4f0646fa93782eefc74f6c60
dinhnhatphan251201 <dinhnhatphan251201@gmail.com>
2022-09-21 17:44:10 +0700

    add

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "Nhat 2 hello 2 an em oi" });
+    res.render("index", { title: "Nhat 2 hello 2 an em oi 2 3 s" });
 });
 
 module.exports = router;
