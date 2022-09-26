c64ba6c74084392914800e5b2fd63b40c6a6eebb
dinhnhatphan251201 <dinhnhatphan251201@gmail.com>
2022-09-26 15:47:24 +0700

    update khoa cntt

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "Hello phan dinh nhat, IUH" });
+    res.render("index", { title: "Hello phan dinh nhat, IUH Khoa CNTT" });
 });
 
 module.exports = router;
